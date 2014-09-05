bb4639d0a522e2007d9a0bf5c15e7c4bd1000afc
unknown <maibarra@697-334-Gimli.sf.k12.internal>
2014-09-05 10:04:15 -0700

    updated entire assignment to have stuff in it

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,56 @@
+int startX, startY, endX, endY, strWeight, redColor, blueColor, greenColor, xPoint;
 void setup()
 {
-  size(300,300);
+  size(600,600);
+  startX = 300;
+  startY = 0;
+  endX = 300;
+  endY = 0;
+  strWeight = 0;
+  background(255);
+  fill(60);
+  textSize(20);
+  text("(click to make your own, press any key to clear)", 67, 520);
+  frameRate(30);
 }
 void draw()
 {
+	redColor = (int)(Math.random()*256);
+	greenColor = (int)(Math.random()*256);
+	blueColor = (int)(Math.random()*256);
+	stroke(redColor, greenColor, blueColor);
+	strokeWeight(strWeight);
+	endX = startX + (int)((Math.random()*10)-5);
+	endY = startY + (int)(Math.random()*17);
+	line(startX, startY, endX, endY);
+	startX = endX;
+	startY = endY;
+	strWeight ++;
+	if (endY > 600)
+	{
+		mousePressed();
+	}
 
 }
 void mousePressed()
 {
-
+	startX = (int)(Math.random()*601);
+ 	startY = 0;
+ 	endX = (int)(Math.random()*601);
+  	endY = 0;
+  	strWeight = 0;
 }
 
+void keyPressed()
+{
+	size(600,600);
+	startX = 300;
+	startY = 0;
+	endX = 300;
+	endY = 0;
+	strWeight = 0;
+	background(255);
+	fill(60);
+	textSize(20);
+	text("(click to make your own, press any key to clear)", 67, 520);
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,62 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int startX, startY, endX, endY, strWeight, redColor, blueColor, greenColor, xPoint;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+  size(600,600);
+  startX = 300;
+  startY = 0;
+  endX = 300;
+  endY = 0;
+  strWeight = 0;
+  background(255);
+  fill(60);
+  textSize(20);
+  text("(click to make your own, press any key to clear)", 67, 520);
+  frameRate(30);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	redColor = (int)(Math.random()*256);
+	greenColor = (int)(Math.random()*256);
+	blueColor = (int)(Math.random()*256);
+	stroke(redColor, greenColor, blueColor);
+	strokeWeight(strWeight);
+	endX = startX + (int)((Math.random()*10)-5);
+	endY = startY + (int)(Math.random()*17);
+	line(startX, startY, endX, endY);
+	startX = endX;
+	startY = endY;
+	strWeight ++;
+	if (endY > 600)
+	{
+		mousePressed();
+	}
+
 }
 public void mousePressed()
 {
-  redraw();
+	startX = (int)(Math.random()*601);
+ 	startY = 0;
+ 	endX = (int)(Math.random()*601);
+  	endY = 0;
+  	strWeight = 0;
 }
 
+public void keyPressed()
+{
+	size(600,600);
+	startX = 300;
+	startY = 0;
+	endX = 300;
+	endY = 0;
+	strWeight = 0;
+	background(255);
+	fill(60);
+	textSize(20);
+	text("(click to make your own, press any key to clear)", 67, 520);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
